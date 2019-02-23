7cfc3e74756782242482521d55b1e113c627df27
itsAmarnath <40494037+itsAmarnath@users.noreply.github.com>
2019-02-23 13:17:44 +0000

    Add files via upload

---
+++ Moveusers_to_another_OU.ps1
@@ -0,0 +1,8 @@
+﻿import-module ActiveDirectory
+$users = import-csv -path C:\Users\ajp13577\book1.csv
+foreach ($user in $users) {  
+$ajp=$User.name3
+#$ajp=(Get-ADUser -Identity $_.ajp3).distinguishedName
+write-host $ajp
+Get-ADUser $ajp | Move-ADObject -TargetPath 'ou=Trainees,dc=ajubachn1,dc=net'
+}
