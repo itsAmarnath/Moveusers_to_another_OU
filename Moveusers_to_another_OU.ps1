0f44ad9e608eb1a3d2415b5e57511471940e5a8d
itsAmarnath <40494037+itsAmarnath@users.noreply.github.com>
2019-02-23 13:27:14 +0000

    Update Moveusers_to_another_OU.ps1

--- Moveusers_to_another_OU.ps1
+++ Moveusers_to_another_OU.ps1
@@ -1,7 +1,7 @@
 import-module ActiveDirectory
-$users = import-csv -path C:\Users\ajp13577\book1.csv
+$users = import-csv -path C:\Users\book1.csv   \\Mention here the path of CSV file. 
 foreach ($user in $users) {  
-$usermove=$User.userid
+$usermove=$User.userid               \\replace userid with name or mail if you want to move users based on name or mail. Remaining other two columns in the CSV can be skipped or left blank.
 Get-ADUser $usermove | Move-ADObject -TargetPath 'ou=Trainees,dc=ajubachn1,dc=net'
 write-host "$usermove moved to new OU"
 }
